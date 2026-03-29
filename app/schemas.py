fb0927b3194d80042113a05536cd5548c5730306
ngbaranov <email@email.com>
2026-03-29 17:57:25 +0300

    second commit

--- app/schemas.py
+++ app/schemas.py
@@ -5,3 +5,8 @@ class Item(BaseModel):
     description: str | None = None
     price: float
     tax: float | None = None
+
+class User(BaseModel):
+    username: str
+    email: str
+    full_name: str | None = None
